b7e02ccf29101eab4a0408c8580ce4e98f9ec097
AlejandroVCwebsite <alejandrocoronaverduchi@students.brillantmont.ch>
2026-09-22 05:20:46 -0600

    Update randomwords.js

--- randomwords.js
+++ randomwords.js
@@ -1,1 +1,9 @@
+onst words = ["apple", "banana", "cherry", "date", "fig", "grape", "kiwi"];
 
+function generateRandomWord() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+
+    const randomWord = words[randomIndex];
+
+    document.getElementById("wordDisplay").innerText = randomWord;
+}
